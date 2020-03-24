0def6e3e5ccfe8703f9b9513b273872962ded54b
tongtong ding <tding7@asu.edu>
2020-03-23 21:20:38 -0700

    drafted index and scores page

---
+++ script.js
@@ -0,0 +1,10 @@
+var start = document.querySelector("#start");
+var questions = document.querySelector("#questions");
+var choiceA = document.querySelector("#A");
+var choiceB = document.querySelector("#B");
+var choiceC = document.querySelector("#C");
+
+
+
+
+
